289979c2d322b524b10d4974ce00a9e6ffb3182d
feiyangqingyun <feiyangqingyun@163.com>
2022-10-27 17:32:10 +0800

    更新作品

---
+++ videowidget/main.cpp
@@ -0,0 +1,35 @@
+﻿#pragma execution_character_set("utf-8")
+
+#include "frmvideowidget.h"
+#include <QApplication>
+#include <QTextCodec>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    QFont font;
+    font.setFamily("Microsoft Yahei");
+    font.setPixelSize(13);
+    a.setFont(font);
+
+#if (QT_VERSION < QT_VERSION_CHECK(5,0,0))
+#if _MSC_VER
+    QTextCodec *codec = QTextCodec::codecForName("gbk");
+#else
+    QTextCodec *codec = QTextCodec::codecForName("utf-8");
+#endif
+    QTextCodec::setCodecForLocale(codec);
+    QTextCodec::setCodecForCStrings(codec);
+    QTextCodec::setCodecForTr(codec);
+#else
+    QTextCodec *codec = QTextCodec::codecForName("utf-8");
+    QTextCodec::setCodecForLocale(codec);
+#endif
+
+    frmVideoWidget w;
+    w.setWindowTitle("视频监控控件 (QQ: 517216493 WX: feiyangqingyun)");
+    w.resize(800, 600);
+    w.show();
+
+    return a.exec();
+}
---
+++ videowidget/videowidget.pro
@@ -0,0 +1,19 @@
+QT += core gui
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+greaterThan(QT_MAJOR_VERSION, 5): QT += core5compat
+
+TARGET      = videowidget
+TEMPLATE    = app
+DESTDIR     = $$PWD/../bin
+CONFIG      += warn_off
+
+SOURCES     += main.cpp
+SOURCES     += frmvideowidget.cpp
+SOURCES     += videowidget.cpp
+
+HEADERS     += frmvideowidget.h
+HEADERS     += videowidget.h
+
+FORMS       += frmvideowidget.ui
+
+RESOURCES += main.qrc
